350b2a29aef099ddd92f022274bb583163ffcc91
unknown <cropyxhtml@gmail.com>
2020-01-28 02:35:26 +0900

    02:35

--- 출근.py
+++ 출근.py
@@ -1,5 +1,9 @@
-import os,datetime
+import os,datetime,re
 working_day = datetime.datetime.now().strftime('%y%m%d_%a')
 if not os.path.isdir(working_day):
     os.mkdir(working_day)
     open(working_day+'/readme.txt','w',encoding='utf-8')
+
+a = os.listdir();print(a)
+년월일 = re.compile('\d{8}',re.VERBOSE)
+print(년월일.search(a));input('wait')
